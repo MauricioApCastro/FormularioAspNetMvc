d99323fe904b3d9213a19f80b112be09c42f5a71
MauricioApCastro <castromac6367@gmail.com>
2022-02-06 09:05:16 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormularioAspNetMvc.Models
+{
+    public class Pessoa
+    {
+        public int CPF { get; set; }
+        public int Nome { get; set; }
+        public int DataNascimento { get; set; }
+        public int Sexo { get; set; }
+        public int EstadoCivil { get; set; }
+        public int Estado { get; set; }
+        public int Cidade { get; set; }
+        public int Logradouro { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int ComplementoEndereco { get; set; }
+        public int Renda { get; set; }
+    }
+}
